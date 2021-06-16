3cbe050c6595fbbd9fd94dae6c18e3ed8e8d8eb9
script-lab <transmit.work@gmail.com>
2021-06-16 13:02:39 +0900

    [add] reducer を導入
    状態管理の hook である useReducer を使用し、コンポーネント内で扱う複数の状態を管理しやすくした。

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import reducer from './reducers/events'
 
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+
+
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body
+    })
+  }
+
 
   return (
     <>
@@ -11,15 +26,15 @@ const App = () => {
         <form>
           <div className="form-group">
             <label htmlFor="formEventTitle">タイトル</label>
-            <input className="form-control" id="formEventTitle"></input>
+            <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}></input>
           </div>
 
           <div className="form-group">
             <label htmlFor="formEventBody">ボディー</label>
-            <input className="form-control" id="formEventBody"></input>
+            <input className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}></input>
           </div>
 
-          <button className="btn btn-primary">イベントを作成する</button>
+          <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
           <button className="btn btn-danger">全てのイベントを削除する</button>
         </form>
 
