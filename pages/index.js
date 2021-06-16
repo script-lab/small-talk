3ee930502de157b2c22fc55ef2e09de82fc5abd7
script-lab <transmit.work@gmail.com>
2021-06-17 06:14:45 +0900

    [add] 操作ログ用の reducer を作成
    redux を用いて、ログの reducer を新たに作成した。

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import React, { useReducer} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppContext from './contexts/AppContext'
-import reducer from './reducers/events'
+import reducer from './reducers'
 import Events from './components/Events'
 import EventForm from './components/EventForm'
 
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
+
+  const initialState = {
+    events: [],
+    operationLogs: []
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
 
   return (
     <AppContext.Provider value={{state, dispatch}}>
